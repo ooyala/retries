1c5229b044c05ec7d3fa9a713104febcd795a83a
Alexander Popov <alex.wayfer@gmail.com>
2018-02-27 12:40:52 +0300

    Update version to 2.0.0

--- lib/retries.rb
+++ lib/retries.rb
@@ -3,7 +3,7 @@
 # Class for retries running
 class Retries
   # Current version
-  VERSION = '1.0.0'
+  VERSION = '2.0.0'
 
   class << self
     # Global (default) options. Can be changed via `#[]` or `#merge!`.
